caf785d970c0158131c2af086b20654d79a112eb
Reid Angwin <rangwin@gmail.com>
2017-09-15 17:27:19 -0400

    19 Rails: More CRUD Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
 
+  get 'questions/index'
+
+  get 'questions/show'
+
+  get 'questions/new'
+
+  get 'questions/create'
+
+  get 'questions/edit'
+
+  get 'questions/update'
+
+  get 'questions/destroy'
+
   resources :posts
 
+  resources :questions
 
   get 'about' => 'welcome#about'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,15 @@ posts = Post.all
   )
 end
 
+50.times do
+  Question.create!(
+      title: RandomData.random_sentence,
+      body:  RandomData.random_paragraph,
+      resolved:  [true, false].sample
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
