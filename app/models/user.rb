684b34341000c56203b5a05900af58230007145b
Reid Angwin <rangwin@gmail.com>
2017-10-16 15:03:20 -0400

    27 Rails: Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
   validates :name, length: {minimum: 1, maximum: 100}, presence: true
 
@@ -15,4 +16,5 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  enum role: [:member, :admin]
 end
