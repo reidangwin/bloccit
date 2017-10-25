937615eb6883ea66ad9278da3357aa4e383fd757
Reid Angwin <rangwin@gmail.com>
2017-10-25 05:45:14 -0400

    31 Rails: Public Profiles Assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def has_posts(user)
+    User.find(user.id).posts.count > 0
+  end
+
+  def has_comments(user)
+    User.find(user.id).comments.count > 0
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Comment, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:comment) {Comment.create!(body: 'Comment Body', post: post, user: user)}
+  let(:comment) { create(:comment, user: user, post: post) }
 
   it {is_expected.to belong_to(:post)}
   it {is_expected.to belong_to(:user)}
@@ -21,7 +21,7 @@ RSpec.describe Comment, type: :model do
 
   describe "after_create" do
     before do
-      @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+      @another_comment = create(:comment, user: user, post: post)
     end
 
     it "sends an email to users who have favorited the post" do
